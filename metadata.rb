4f1bdceffbd06e28de3cfe667c9f127aa234e3c7
Björn Zettergren <bjorn.zettergren@deltaprojects.com>
2015-11-10 14:47:00 +0100

    bumped version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chef@julienpellet.com'
 license          'Apache 2.0'
 description      'Installs/Configures ambari'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.1'
+version          '0.3.2'
 supports 'redhat', '>= 5.0'
 supports 'centos', '>= 5.0'
 supports 'amazon', '>= 5.0'
