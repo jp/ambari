ee01046e7d441b9272e4fcd159c78bd16f8665f6
Björn Zettergren <bjorn.zettergren@deltaprojects.com>
2016-06-27 11:05:27 +0200

    added a block that removes 127.0.1.1 from hosts
    Debian and ubuntu distros adds a entry in /etc/hosts that points the
    hostname to 127.0.0.1, this won't work properly with ambari or hadoop so
    we remove it here, even though this might be better off in a
    wrapper-cookbook.
    
    All of these changes was initially written by @Raboo some time ago. I
    just merged changes to @jp's master.

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,3 +16,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+case node['lsb']['codename']
+when 'precise', 'trusty', 'wheezy'
+  ruby_block "remove 127.0.1.1 /etc/hosts" do
+    block do
+      file = Chef::Util::FileEdit.new("/etc/hosts")
+      file.search_file_delete_line(/^127\.0\.1\.1/)
+      file.write_file
+    end
+  end
+end
