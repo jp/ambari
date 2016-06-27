607d6658ad08ff2b1cefa46b0efa0b6dbdc3a4a8
Björn Zettergren <bjorn.zettergren@deltaprojects.com>
2016-06-27 10:43:11 +0200

    Fixed repository urls and added default db-params
    The debian urls were fixed to support handling by the 'apt' cookbook,
    which is used in in the 'setup_package_manager' recipe.
    
    Also added mechanics for specifying database-parameters and being able
    to eventually select postgres/mysql/other database types in the future.
    Currently only Postgres (embedded) and Mysql is supported, for
    ubuntu/debian only.
    
    All of these changes was initially written by @Raboo some time ago. I
    just merged changes to @jp's master.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,8 +3,10 @@
 # Attributes:: default
 #
 
-# Set default version of Ambari if one is not supplied.
-node.default['ambari']['version'] = '2.2.0'
+# node.default['ambari']['server_fqdn'] = 'localhost'
+
+# Set default version of Ambari.  Overwrite this to install a different version.
+node.default['ambari']['version'] = '2.2.1'
 
 case node['ambari']['version']
 when '1.7'
@@ -29,23 +31,35 @@ when '2.1'
   node.default['ambari']['suse_11_repo'] = 'http://public-repo-1.hortonworks.com/ambari/suse11/2.x/updates/2.1.2/ambari.repo'
   node.default['ambari']['ubuntu_12_repo'] = 'http://public-repo-1.hortonworks.com/ambari/ubuntu12/2.x/updates/2.1.2.1'
   node.default['ambari']['ubuntu_14_repo'] = 'http://public-repo-1.hortonworks.com/ambari/ubuntu14/2.x/updates/2.1.2.1'
-  node.default['ambari']['debian_7_repo'] = 'http://public-repo-1.hortonworks.com/ambari/debian7/2.x/updates/2.1.2/ambari.list'
+  node.default['ambari']['debian_7_repo'] = 'http://public-repo-1.hortonworks.com/ambari/debian7/2.x/updates/2.1.2'
 when '2.2', '2.2.0'
   node.default['ambari']['rhel_6_repo'] = 'http://public-repo-1.hortonworks.com/ambari/centos6/2.x/updates/2.2.0.0/ambari.repo'
   node.default['ambari']['rhel_7_repo'] = 'http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.2.0.0/ambari.repo'
   node.default['ambari']['suse_11_repo'] = 'http://public-repo-1.hortonworks.com/ambari/suse11/2.x/updates/2.2.0.0/ambari.repo'
   node.default['ambari']['ubuntu_12_repo'] = 'http://public-repo-1.hortonworks.com/ambari/ubuntu12/2.x/updates/2.2.0.0'
   node.default['ambari']['ubuntu_14_repo'] = 'http://public-repo-1.hortonworks.com/ambari/ubuntu14/2.x/updates/2.2.0.0'
-  node.default['ambari']['debian_7_repo'] = 'http://public-repo-1.hortonworks.com/ambari/debian7/2.x/updates/2.2.0.0/ambari.list'
+  node.default['ambari']['debian_7_repo'] = 'http://public-repo-1.hortonworks.com/ambari/debian7/2.x/updates/2.2.0.0'
 when '2.2.1'
   node.default['ambari']['rhel_6_repo'] = 'http://public-repo-1.hortonworks.com/ambari/centos6/2.x/updates/2.2.1.1/ambari.repo'
   node.default['ambari']['rhel_7_repo'] = 'http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.2.1.1/ambari.repo'
   node.default['ambari']['suse_11_repo'] = 'http://public-repo-1.hortonworks.com/ambari/suse11/2.x/updates/2.2.1.1/ambari.repo'
   node.default['ambari']['ubuntu_12_repo'] = 'http://public-repo-1.hortonworks.com/ambari/ubuntu12/2.x/updates/2.2.1.1'
   node.default['ambari']['ubuntu_14_repo'] = 'http://public-repo-1.hortonworks.com/ambari/ubuntu14/2.x/updates/2.2.1.1'
-  node.default['ambari']['debian_7_repo'] = 'http://public-repo-1.hortonworks.com/ambari/debian7/2.x/updates/2.2.1.1/ambari.list'
+  node.default['ambari']['debian_7_repo'] = 'http://public-repo-1.hortonworks.com/ambari/debian7/2.x/updates/2.2.1.1'
 end
-
 
 node.default['ambari']['admin_user'] = 'admin'
 node.default['ambari']['admin_password'] = 'admin'
+
+# DB attributes
+node.default['ambari']['database']['type'] = 'embedded' # embedded|mysql (can support following in the future oracle|mssql|postgres|sqlanywhere)
+# These attributes below are omittied if database type is 'embedded'.
+node.default['ambari']['database']['host'] = 'localhost'
+node.default['ambari']['database']['port'] = '3306'
+node.default['ambari']['database']['name'] = 'ambari'
+node.default['ambari']['database']['username'] = 'ambari'
+node.default['ambari']['database']['password'] = 'bigdata'
+
+# JDBC attributes, will only be used if url is specified and db type isn't 'embedded'.
+node.default['ambari']['jdbc']['url'] = '' # leave blank if no remote file
+node.default['ambari']['jdbc']['path'] = '/usr/share/java/mysql-connector-java.jar'
