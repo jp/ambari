1370c57045118eec2864b51c226a4f9f1b0a1ec8
Chris Gianelloni <chris@cask.co>
2016-12-06 19:09:04 -0500

    Add issues_url and source_url for foodcritic

--- metadata.rb
+++ metadata.rb
@@ -5,10 +5,15 @@ license          'Apache 2.0'
 description      'Installs/Configures ambari'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.3.3'
+
 supports 'redhat', '>= 5.0'
 supports 'centos', '>= 5.0'
 supports 'amazon', '>= 5.0'
 supports 'scientific', '>= 5.0'
 supports 'suse', '>= 11.0'
 supports 'ubuntu', '>= 12.0'
+
 depends 'apt'
+
+source_url 'https://github.com/jp/ambari' if respond_to?(:source_url)
+issues_url 'https://github.com/jp/ambari/issues' if respond_to?(:issues_url)
